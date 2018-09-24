83565c72783c64fe14979c7b9e454b921604c71e
w0utje <wouterschol@hotmail.com>
2018-09-24 12:00:15 +0200

    added sponsored fees and aliases

--- appng.js
+++ appng.js
@@ -2,27 +2,28 @@ var request = require('sync-request');
 var fs = require('fs');
 
 /**
- * V 2.0.2
+ * V 2.0.3
  * w0utje's edit of Hawky's LPoSDistributor
  * added mercury and ripto bux fee calculation
- * added (cancelled) leases infomation to be re-used next payout 
+ * added (cancelled) leases infomation to be re-used next payout
  * added HTML payout overview generation
  * added feeAssetId to the payments for sending payments with a custom assetFee
  * Removed 0.003 Waves Fee substraction, because of the custom assetFee adding
- * if you don't want to use an assetFee, remember that you'll need to substract the waves fee for each transaction.   
- * Asset fee's that aren't dividable (since there aren't any decimals left) 
- * could cause strange asset fee splitting (no 60-40). 
+ * if you don't want to use an assetFee, remember that you'll need to substract the waves fee for each transaction.
+ * Asset fee's that aren't dividable (since there aren't any decimals left)
+ * could cause strange asset fee splitting (no 60-40).
  * mercury and ripto bux both have 8 decimals and are sent with 1 decimal, so there's plenty of space to divide those assets.
  * keep this in mind when adding your own asset fee's
- * 
- *    
+ * Added, sponsored fees and aliases
+ *
+ *
  * Put your settings here:
  *     - address: the address of your node that you want to distribute from
  *     - startBlockHeight: the block from which you want to start distribution for
  *     - endBlock: the block until you want to distribute the earnings
  *     - distributableMRTPerBlock: amount of MRT distributed per forged block
  *     - filename: file to which the payments for the mass payment tool are written
- *     - paymentid: used to create payment and html file with this id.   
+ *     - paymentid: used to create payment and html file with this id.
  *     - node: address of your node in the form http://<ip>:<port
  *     - assetFeeId: AssetID used as fee for payments, empty or null for using waves
  *     - feeAmount: fee amount counted from decimals. example: asset with 2 decimals. fee=1 => 0.01
@@ -31,19 +32,19 @@ var fs = require('fs');
  */
 var config = {
     address: '3PEFQiFMLm1gTVjPdfCErG8mTHRcH2ATaWa',
-    startBlockHeight: 805000,
-    endBlock: 808660,
+    startBlockHeight: 1150070,
+    endBlock: 1160406,
     distributableMrtPerBlock: 0,  //MRT distribution stopped
     filename: 'payment', //.json added automatically
-    paymentid: "36",
+    paymentid: "xx",
     node: 'http://127.0.0.1:6869',
     //node: 'http://nodes.wavesnodes.com',
-    assetFeeId: "5BK9HPKmSkxoMdqvDzneb2UaW2NzDRjoMpMvQWfB4NcK",
-    feeAmount: 1,  
+    assetFeeId: null, //not used anymore with sponsored tx
+    feeAmount: 1,
     paymentAttachment: "DVCsMf2Av2pvvM8GNzzP1tQKZtd4jWfcHJQj9bky32RR6janfLK2", //thank you for leasing to bearwaves...
     percentageOfFeesToDistribute: 100
 };
- 
+
 
 var myLeases = {};
 var myCanceledLeases = {};
@@ -53,7 +54,7 @@ var currentStartBlock = 462000;
 
 var fs=require('fs');
 var prevleaseinfofile = config.startBlockHeight + "_" + config.address + ".json";
-if (fs.existsSync(prevleaseinfofile)) 
+if (fs.existsSync(prevleaseinfofile))
 {
 	console.log("reading" + prevleaseinfofile + " file");
 	var data=fs.readFileSync(prevleaseinfofile);
@@ -75,7 +76,7 @@ for(var cancelindex in myCanceledLeases)
         cleancount++;
     }
 
-}  
+}
 console.log("done cleaning, removed: " + cleancount);
 
 var payments = [];
@@ -83,6 +84,7 @@ var mrt = [];
 var merfees=[];
 var rbxfees=[];
 
+var myAliases = [];
 
 var BlockCount = 0;
 
@@ -97,6 +99,8 @@ var myForgedBlocks = [];
   * masspayment tool.
  */
 var start = function() {
+  console.log('get aliases');
+  myAliases = getAllAlias();
     console.log('getting blocks...');
     var blocks = getAllBlocks();
     console.log('preparing datastructures...');
@@ -114,7 +118,7 @@ var start = function() {
     });
     //Get last block
     LastBlock = blocks.slice(-1)[0] ;
-	
+
     pay();
     console.log("blocks forged: " + BlockCount);
 };
@@ -125,7 +129,7 @@ var start = function() {
  *
  *   @param blocks all blocks that should be considered
  */
- 
+
 var prepareDataStructure = function(blocks) {
 
     blocks.forEach(function(block,index) {
@@ -135,7 +139,7 @@ var prepareDataStructure = function(blocks) {
         var merFees = 0;
         var rbxFees = 0;
 
-        if (block.generator === config.address) 
+        if (block.generator === config.address)
         {
             myForgedBlocks.push(block);
             checkprevblock = true;
@@ -145,41 +149,44 @@ var prepareDataStructure = function(blocks) {
 		var blockmerfees=0;
 		var blockrbxfees=0;
 
-        block.transactions.forEach(function(transaction) 
+        block.transactions.forEach(function(transaction)
         {
             // type 8 are leasing tx
-            if (transaction.type === 8 && transaction.recipient === config.address) {
+            if (transaction.type === 8 && ((transaction.recipient === config.address)|| (myAliases.indexOf(transaction.recipient) > -1) )){
                 transaction.block = block.height;
                 myLeases[transaction.id] = transaction;
             } else if (transaction.type === 9 && myLeases[transaction.leaseId]) { // checking for lease cancel tx
                 transaction.block = block.height;
                 myCanceledLeases[transaction.leaseId] = transaction;
-            } 
+            }
 
 			if(myblock)
 			{
                 // considering Waves fees
-                if (!transaction.feeAsset || transaction.feeAsset === '' || transaction.feeAsset === null) 
+                if (!transaction.feeAsset || transaction.feeAsset === '' || transaction.feeAsset === null)
                 {
                     if(transaction.fee < 200000000) // if tx waves fee is more dan 2 waves, filter it. probably a mistake by someone
                     {
                         //wavesFees += (transaction.fee*0.4);
                         blockwavesfees += transaction.fee;
-                        
+
                     } else {
                         console.log("Filter TX at block: " + block.height + " Amount: " +  transaction.fee)
-                    }   
-                } 
-            
+                    }
+                } else if (block.height > 1090000 && transaction.type === 4) {
+                blockwavesfees += 100000;
+								}
 
+                /*
                 if (transaction.feeAsset === 'HzfaJp8YQWLvQG4FkUxq2Q7iYWMYQ2k8UF89vVJAjWPj') {     //Mercury
                     //merFees += (transaction.fee*0.4);
                     blockmerfees += transaction.fee;
-                }       
+                }
                 if (transaction.feeAsset === 'AnERqFRffNVrCbviXbDEdzrU6ipXCP5Y1PKpFdRnyQAy') {     //Ripto Bux
                     //rbxFees += (transaction.fee*0.4);
                     blockrbxfees += transaction.fee;
-                }     
+                }
+                */
 			}
       });
       wavesFees += Math.round(parseInt(blockwavesfees / 5) * 2);
@@ -189,14 +196,14 @@ var prepareDataStructure = function(blocks) {
       blockwavesfees=0;
       blockmerfees=0;
       blockrbxfees=0;
-        
-      if(checkprevblock)  
-      {      
-        if (index > 0) 
+
+      if(checkprevblock)
+      {
+        if (index > 0)
         {
             //console.log("Next: " + blocks[index + 1]);
             var prevblock = blocks[index - 1];
-            prevblock.transactions.forEach(function(transaction) 
+            prevblock.transactions.forEach(function(transaction)
             {
                 // considering Waves fees
                 if (!transaction.feeAsset || transaction.feeAsset === '' || transaction.feeAsset === null) {
@@ -207,29 +214,33 @@ var prepareDataStructure = function(blocks) {
                 } else {
   			        console.log("Filter TX at block: " + block.height + " Amount: " +  transaction.fee)
   		        }
-            } 
-              
+            } else if (block.height > 1090000 && transaction.type === 4) {
+                blockwavesfees += 100000;
+								}
+
+              /*
               if (transaction.feeAsset === 'HzfaJp8YQWLvQG4FkUxq2Q7iYWMYQ2k8UF89vVJAjWPj') {     //Mercury
                   //merFees += (transaction.fee*0.6);
                   blockmerfees += transaction.fee;
-              }       
+              }
               if (transaction.feeAsset === 'AnERqFRffNVrCbviXbDEdzrU6ipXCP5Y1PKpFdRnyQAy') {     //Ripto Bux
                   //rbxFees += (transaction.fee*0.6);
                   blockrbxfees += transaction.fee;
-              } 
-            });        
-        }    
-				
+              }
+              */
+            });
+        }
+
       wavesFees += (blockwavesfees - Math.round(parseInt(blockwavesfees / 5) * 2));
       merFees += (blockmerfees - Math.round(parseInt(blockmerfees / 5) * 2));
-      rbxFees += (blockrbxfees - Math.round(parseInt(blockrbxfees / 5) * 2));				
-				        
-      } 
+      rbxFees += (blockrbxfees - Math.round(parseInt(blockrbxfees / 5) * 2));
+
+      }
 
         block.wavesFees = wavesFees;
         block.merFees = merFees;
         block.rbxFees = rbxFees;
-         
+
     });
 };
 
@@ -262,7 +273,7 @@ var getAllBlocks = function() {
                 }
             }).getBody('utf8'));
         }
-        currentBlocks.forEach(function(block) 
+        currentBlocks.forEach(function(block)
         {
             if (block.height <= config.endBlock) {
                 blocks.push(block);
@@ -278,6 +289,29 @@ var getAllBlocks = function() {
 
     return blocks;
 };
+
+
+/**
+ * Method that returns all aliases for address.
+ *
+ * @returns {Array} all aliases for address
+ */
+var getAllAlias = function() {
+
+						var AliasArr = [];
+            var Aliases = JSON.parse(request('GET', config.node + '/alias/by-address/' + config.address, {
+                'headers': {
+                    'Connection': 'keep-alive'
+                }
+            }).getBody('utf8'));
+
+        Aliases.forEach(function(alias)
+        {
+						 AliasArr.push(alias);
+						 console.log(alias);
+        });
+    return AliasArr;
+}
 
 /**
  * This method distributes either Waves fees and MRT to the active leasers for
@@ -297,10 +331,10 @@ var distribute = function(activeLeases, amountTotalLeased, block) {
         var amount = fee * share;
         var meramount = merfee * share;
         var rbxamount = rbxfee * share;
-        
+
         var assetamounts = [];
 
-        
+
         var amountMRT = share * config.distributableMrtPerBlock;
 
         if (address in payments) {
@@ -308,8 +342,8 @@ var distribute = function(activeLeases, amountTotalLeased, block) {
             mrt[address] += amountMRT;
             merfees[address] +=  meramount * (config.percentageOfFeesToDistribute / 100);
             rbxfees[address] +=  rbxamount * (config.percentageOfFeesToDistribute / 100);
-            
-            
+
+
         } else {
             payments[address] = amount * (config.percentageOfFeesToDistribute / 100);
             mrt[address] = amountMRT;
@@ -331,13 +365,13 @@ var pay = function() {
     var totalfees =0;
     var totalmerfees=0;
     var totalrbxfees=0;
-    
+
     var html = "";
-    
+
     var html = "<!DOCTYPE html>" +
 "<html lang=\"en\">" +
 "<head>" +
-"  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">" + 
+"  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">" +
 "  <link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css\">" +
 "  <script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js\"></script>" +
 "  <script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js\"></script>" +
@@ -349,7 +383,7 @@ var pay = function() {
 "  <h4>(LPOS address: " + config.address + ")</h4>" +
 "  <h5>29-06-2017: Hi all, again a short update of the fee's earned by the bearwaves node. Automated distribution, riding on BearWaves $BEAR. Cheers!</h5> " +
 "  <h5>You can always contact us by <a href=\"mailto:bearwaves@outlook.com\">E-mail</a> or in Waves Slack @w0utje <img src=\"https://www.bearwaves.nl/wp-content/uploads/2017/08/banksy.jpg\" style=\"width:50px;height:50px;\"></h5>" +
-"  <h5>Blocks forged: " + BlockCount + "</h5>" + 
+"  <h5>Blocks forged: " + BlockCount + "</h5>" +
 "  <table class=\"table table-striped table-hover\">" +
 "    <thead> " +
 "      <tr>" +
@@ -357,12 +391,12 @@ var pay = function() {
 "        <th>Waves</th>" +
 "        <th>MRT</th>" +
 "        <th>Mercury</th>" +
-"        <th>Ripto Bux</th>" +        
-        
+"        <th>Ripto Bux</th>" +
+
 "      </tr>" +
 "    </thead>" +
 "    <tbody>";
-    
+
     for (var address in payments) {
         var payment = (payments[address] / Math.pow(10, 8));
         console.log(address + ' will receive ' + parseFloat(payment).toFixed(8) + ' and ' + parseFloat(mrt[address]).toFixed(2) + ' MRT and ' + parseFloat(merfees[address]).toFixed(8) + ' Mercury!');
@@ -370,8 +404,8 @@ var pay = function() {
         if (Number(Math.round(payments[address])) > 0) {
             transactions.push({
                 "amount": Number(Math.round(payments[address])),
-               	"fee": config.feeAmount, 
-                "feeAssetId": config.assetFeeId,  
+               	"fee": config.feeAmount,
+                //"feeAssetId": config.assetFeeId,
                 "sender": config.address,
                 "attachment": config.paymentAttachment,
                 "recipient": address
@@ -381,8 +415,8 @@ var pay = function() {
         if (Number(Math.round(mrt[address] * Math.pow(10, 2))) > 0) {
             transactions.push({
                 "amount": Number(Math.round(mrt[address] * Math.pow(10, 2))),
-               	"fee": config.feeAmount, 
-                "feeAssetId": config.assetFeeId, 
+               	"fee": config.feeAmount,
+                //"feeAssetId": config.assetFeeId,
                 "assetId": "4uK8i4ThRGbehENwa6MxyLtxAjAo1Rj9fduborGExarC",
                 "sender": config.address,
                 "attachment": config.paymentAttachment,
@@ -393,59 +427,59 @@ var pay = function() {
         if (Number(Math.round(merfees[address])) > 0) {
             transactions.push({
                 "amount": Number(Math.round(merfees[address])),
-               	"fee": config.feeAmount, 
-                "feeAssetId": config.assetFeeId,
+               	"fee": config.feeAmount,
+                //"feeAssetId": config.assetFeeId,
                 "assetId": "HzfaJp8YQWLvQG4FkUxq2Q7iYWMYQ2k8UF89vVJAjWPj",
                 "sender": config.address,
                 "attachment": config.paymentAttachment,
                 "recipient": address
             });
-        }   
+        }
         //this will send one BearWaves token to every leaser
             transactions.push({
                 "amount": 100,
-               	"fee": config.feeAmount, 
-                "feeAssetId": config.assetFeeId, 
+               	"fee": config.feeAmount,
+                //"feeAssetId": config.assetFeeId,
                 "assetId": "9gnc5UCY6RxtSi9FEJkcD57r5NBgdr45DVYtunyDLrgC",
                 "sender": config.address,
                 "attachment": config.paymentAttachment,
                 "recipient": address
-            });           
-        
+            });
+
         totalMRT += mrt[address];
         totalfees += payments[address];
         totalmerfees += merfees[address];
         totalrbxfees += rbxfees[address];
-        
 
-        
+
+
         //html += "<tr><td>" + address + "</td><td>" + ((payments[address]/100000000).toPrecision(8) - 0.002) + "</td><td>" + (merfees[address]/100000000).toPrecision(8) + "</td><td>" + mrt[address].toPrecision(8) + "</td><td>" + (upfees[address]/100000000) + "</td></tr>\r\n";
         html += "<tr><td>" + address + "</td><td>" + 							 	//address column
 				((payments[address]/100000000).toFixed(8)) + "</td><td>" + 	//Waves fee's
 				mrt[address].toFixed(2) + "</td><td>" +                     //MRT
 				(merfees[address]/100000000).toFixed(8) + "</td><td>" +		//Mercury fee's
-				(rbxfees[address]/100000000).toFixed(8) + "</td></tr>" +		//Ripto Bux fee's 
-				
+				(rbxfees[address]/100000000).toFixed(8) + "</td></tr>" +		//Ripto Bux fee's
+
 				"\r\n";
     }
-    
+
     html += "<tr><td><b>Total</b></td><td><b>" + ((totalfees/100000000).toFixed(8)) +
 		 "</b></td><td><b>" + totalMRT.toFixed(2) + "</b></td><td><b>" +
 		  (totalmerfees/100000000).toFixed(8) + "</b></td><td><b>" +
-		  (totalrbxfees/100000000).toFixed(8) + "</b></td></tr>" +		  
+		  (totalrbxfees/100000000).toFixed(8) + "</b></td></tr>" +
 			"\r\n";
-    
+
     html += "</tbody>" +
 "  </table>" +
 "</div>" +
 
 "</body>" +
 "</html>";
-    
+
     console.log("total fees: " + (totalfees/100000000) + " total MRT: " + totalMRT + " total Mer: " + (totalmerfees/100000000) + " total Up: " + (totalrbxfees/100000000) );
     var paymentfile = config.filename + config.paymentid + ".json";
     var htmlfile = config.filename + config.paymentid + ".html";
-    
+
     fs.writeFile(paymentfile, JSON.stringify(transactions), {}, function(err) {
         if (!err) {
             console.log('payments written to ' + paymentfile + '!');
@@ -453,39 +487,39 @@ var pay = function() {
             console.log(err);
         }
     });
-    
+
     fs.writeFile(htmlfile, html, {}, function(err) {
         if (!err) {
             console.log('html written!');
         } else {
             console.log(err);
         }
-    });    
-    
+    });
+
     var latestblockinfo = {};
     latestblockinfo["leases"]=myLeases;
     latestblockinfo["canceledleases"]=myCanceledLeases;
     var blockleases = config.endBlock + "_" + config.address + ".json" ;
-    
+
     fs.writeFile(blockleases, JSON.stringify(latestblockinfo), {}, function(err) {
         if (!err) {
             console.log('leaseinfo written to ' + blockleases + '!');
         } else {
             console.log(err);
         }
-    });    
-     
+    });
+
     var ActiveLeaseData = getActiveLeasesAtBlock(LastBlock);
-		
+
     fs.writeFile("LastBlockLeasers.json", JSON.stringify(ActiveLeaseData), {}, function(err) {
         if (!err) {
             console.log('ActiveLeasers written to LastBlockLeasers.json!');
         } else {
             console.log(err);
         }
-    });      
-    
-    
+    });
+
+
 };
 
 /**
@@ -523,4 +557,3 @@ var getActiveLeasesAtBlock = function(block) {
 };
 
 start();
-
